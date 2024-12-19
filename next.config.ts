26beeb0e0afaaf027592eb59c524464250723322
Elisha Mallard <elishamallard@Elishas-MacBook-Pro-2.local>
2024-12-19 08:07:02 +0200

    resolution

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,4 @@
 import type { NextConfig } from "next";
-const withFonts = require('next-fonts');
-
-/** @type {import('next').NextConfig} */
-// const nextConfig = withFonts({});
 
 const nextConfig: NextConfig = { 
     reactStrictMode: true,
